cd97b4d6bb786c183aa650337839936f6ccc6cf5
Ryzzoxxy <163017172+Ryzzoxxy@users.noreply.github.com>
2025-01-20 20:53:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.getElementById("saveProfile").addEventListener("click", function () {
+    const username = document.getElementById("username").value.trim();
+    if (!username) {
+        alert("Please enter a valid username!");
+        return;
+    }
+
+    // Sauvegarder les données dans le localStorage
+    const profileData = { username: username };
+    localStorage.setItem(username, JSON.stringify(profileData));
+
+    // Redirection vers la page du profil
+    const profileLink = `https://votrenom.github.io/profil.html?username=${username}`;
+    window.location.href = profileLink;
+});
 
